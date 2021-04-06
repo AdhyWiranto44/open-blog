ab44589841749670942c5a88c174a6003bedea39
AdhyWiranto44 <adhywiranto68@gmail.com>
2021-04-06 16:50:47 +0700

    Adding auth UI

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.render("frontend");
+    res.render("frontend", {title: "Open Blog", tag: ""});
 })
 
 app.get("/documentation", (req, res) => {
@@ -15,7 +15,23 @@ app.get("/documentation", (req, res) => {
 })
 
 app.get("/post/post-title", (req, res) => {
-    res.render("post-page");
+    res.render("post-page", {title: "Open Blog: Post"});
+})
+
+app.get("/tag/post-tag", (req, res) => {
+    res.render("frontend", {title: "Open Blog: Post Tag", tag: "nodejs"});
+})
+
+app.get("/auth/login", (req, res) => {
+    res.render("login", {title: "Open Blog: Login"})
+})
+
+app.get("/auth/register", (req, res) => {
+    res.render("register", {title:"Open Blog: Register"})
+})
+
+app.get("/auth/reset-password", (req, res) => {
+    res.render("reset-password", {title: "Open Blog: Reset Password"})
 })
 
 app.listen(3000, () => {
