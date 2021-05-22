4b6615c866522c272b5a5ef7f0d7a658d8c9b9cc
adhywiranto44 <adhywiranto68@gmail.com>
2021-05-22 13:33:21 +0700

    Memperlebar ukuran form tambah dan ubah post

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ const post1 = Post({
     title: "Post 1",
     slug: "post-1",
     content: "Vehicula ipsum a arcu cursus vitae congue mauris rhoncus aenean. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non. Scelerisque eleifend donec pretium vulputate sapien nec sagittis aliquam. Nullam vehicula ipsum a arcu cursus vitae congue mauris. Cum sociis natoque penatibus et magnis dis. Nunc consequat interdum varius sit amet. Quisque egestas diam in arcu cursus euismod. Scelerisque felis imperdiet proin fermentum leo. Netus et malesuada fames ac turpis egestas integer eget. Vel fringilla est ullamcorper eget nulla facilisi etiam dignissim. Id interdum velit laoreet id donec ultrices tincidunt.",
-    img: "post-img.jpg",
+    img: "post-img2.jpg",
     tags: ["nodejs", "express", "bootstrap", "todayilearn"],
     author: "Admin",
     active: 1,
