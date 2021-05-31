2dd94f001e3f8225483c7254257271324feaf29c
adhywiranto44 <adhywiranto68@gmail.com>
2021-05-31 08:40:07 +0700

    change port

--- app.js
+++ app.js
@@ -640,6 +640,6 @@ app.route("/admin/mengubah-post/:postSlug")
         })
     })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("http://localhost:3000");
 })
