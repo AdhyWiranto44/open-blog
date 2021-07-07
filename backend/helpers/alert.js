6fa24c991dedc01d979c1802feaf7802277d9da6
adhywiranto44 <adhywiranto68@gmail.com>
2021-07-07 20:31:41 +0700

    delete all

--- backend/helpers/alert.js
+++
@@ -1,5 +0,0 @@
-const showAlert = function(color, message) {
-    return `<div class="alert ${color} alert-dismissible fade show shadow-sm" role="alert">${message}<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>`;
-}
-
-module.exports = showAlert;
--- backend/helpers/dates.js
+++
@@ -1,4 +0,0 @@
-const arrDay = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jum'at", "Sabtu"];
-const arrMonth = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"];
-
-module.exports = {arrDay, arrMonth};
