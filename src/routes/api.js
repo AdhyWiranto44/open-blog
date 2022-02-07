eed398d1dbd36fcb9dcae976e6e96dc4d2ac5894
Adhy Wiranto <30892092+AdhyWiranto44@users.noreply.github.com>
2022-02-07 14:21:39 +0700

    del: remove home router from api.js

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
 
-const homeRouter = require('./home_router');
-const dashboardRouter = require('./dashboard_router');
 const postRouter = require('./post_router');
 const tagRouter = require('./tag_router');
 const commentRouter = require('./comment_router');
@@ -10,8 +8,6 @@ const loginRouter = require('./login_router');
 const userRouter = require('./user_router');
 
 
-app.use(homeRouter);
-app.use(dashboardRouter);
 app.use(tagRouter);
 app.use(postRouter);
 app.use(commentRouter);
