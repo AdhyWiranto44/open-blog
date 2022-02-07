6e6b8c6d3e7deb610ded0448561520cbe0bce11e
Adhy Wiranto <30892092+AdhyWiranto44@users.noreply.github.com>
2022-02-07 14:21:15 +0700

    del: home router and controller

--- src/routes/home_router.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const homeController = require('../controllers/home_controller');
-
-
-router.get("/", homeController.index);
-router.post("/", homeController.find);
-
-
-module.exports = router;
