7fc85a7ad7e7ec2078bac27c60235a2c9cb5562a
Adhy Wiranto <30892092+AdhyWiranto44@users.noreply.github.com>
2022-02-07 13:34:52 +0700

    del: menghapus route dan controller dashboard

--- src/routes/dashboard_router.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const DashboardController = require('../controllers/dashboard_controller');
-
-
-router.get("/dashboard", new DashboardController().index);
-
-
-module.exports = router;
