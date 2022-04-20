b6a4fa4cb4d507a267372302c9f0741c702c4151
Adhy Wiranto <30892092+AdhyWiranto44@users.noreply.github.com>
2022-04-20 16:29:20 +0700

    feat: frontend, cookie for dashboard page

--- client/pages/admin/dashboard.js
+++ client/pages/admin/dashboard.js
@@ -1,13 +1,21 @@
 // import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 import BackendLayout from "../../layouts/backend";
+import Cookies from "js-cookie";
+import { useRouter } from "next/router";
 
 
 export default function Dashboard() {
-    return (
-        <BackendLayout>
-            <h1>Dashboard</h1>
-        </BackendLayout>
-    );
+  const router = useRouter();
+  const token = Cookies.get("X-OPEN-BLOG-TOKEN");
+  if (!token || token === "") {
+    return router.push("/login");
+  }
+
+  return (
+    <BackendLayout>
+      <h1>Dashboard</h1>
+    </BackendLayout>
+  );
 }
 
 // export const getServerSideProps = withPageAuthRequired();
