07465f2c43cb57c1a4eef11d29fbaf32c4c98c57
Adhy Wiranto <30892092+AdhyWiranto44@users.noreply.github.com>
2022-04-23 16:08:49 +0700

    fix: Form should be filled for user registration

--- src/controllers/user_controller.ts
+++ src/controllers/user_controller.ts
@@ -5,245 +5,254 @@ import ApiResponse from '../helpers/api_response';
 
 
 class UserController {
-    constructor() {}
+  constructor() { }
 
-    async getUsers(req, res) {
-        const userLogin = req.query.token;
+  async getUsers(req, res) {
+    const userLogin = req.query.token;
 
-        // if not logged in and find archived post
-        if (typeof userLogin === 'undefined') {
-            return new ApiResponse(
-                res, 401, false, 
-                `Please login first to get all users.`
-            ).sendResponse();
-        }
-
-        try {
-            let users = [];
-
-            await User.find({}).exec()
-                .then(foundUsers => {
-                    users = foundUsers;
-                });
-
-            // if users not found
-            if (users.length < 1) {
-                return new ApiResponse(
-                    res, 404, false, 
-                    `Users not found.`
-                ).sendResponse();
-            }
-
-            return new ApiResponse(
-                res, 200, true, 
-                `Found users.`,
-                { users }
-            ).sendResponse();
-        } catch(err) {
-            return new ApiResponse(
-                res, 500, false, 
-                err.message
-            ).sendResponse();
-        }
+    if (typeof userLogin === 'undefined') {
+      return new ApiResponse(
+        res, 401, false,
+        `Please login first to get all users.`
+      ).sendResponse();
     }
 
-    async getUser(req, res) {
-        interface User {
-            username: string,
-            password: string,
-            img: string,
-            created_at: Date,
-            updated_at: Date,
-        }
+    try {
+      let users = [];
 
-        const userLogin = req.session.username;
-        const username = req.params.username;
+      await User.find({}).exec()
+        .then(foundUsers => {
+          users = foundUsers;
+        });
 
-        if (typeof userLogin === 'undefined') {
-            return new ApiResponse(
-                res, 401, false, 
-                `Please login first to get user.`
-            ).sendResponse();
-        }
-    
-        try {
-            let user = null;
-            await User.findOne({username}).exec()
-            .then(foundUser => {
-                user = foundUser;
-            });
+      // if users not found
+      if (users.length < 1) {
+        return new ApiResponse(
+          res, 404, false,
+          `Users not found.`
+        ).sendResponse();
+      }
 
-            if (user == null) {
-                return new ApiResponse(
-                    res, 404, false, 
-                    `User not found.`
-                ).sendResponse();
-            }
-
-            return new ApiResponse(
-                res, 200, true, 
-                `Found user.`,
-                { user }
-            ).sendResponse();
-        } catch(err) {
-            return new ApiResponse(
-                res, 500, false, 
-                err.message
-            ).sendResponse();
-        }
+      return new ApiResponse(
+        res, 200, true,
+        `Found users.`,
+        { users }
+      ).sendResponse();
+    } catch (err) {
+      return new ApiResponse(
+        res, 500, false,
+        err.message
+      ).sendResponse();
     }
-    
-    async insertUser(req, res) {
-        const userLogin: string = req.query.token;
-        const formInput: any = {
-            username: req.body.username,
-            password: req.body.password,
-            confirm_password: req.body.confirm_password
-        }
+  }
 
-        if (typeof userLogin === 'undefined') {
-            return new ApiResponse(
-                res, 401, false, 
-                `Please login first to insert new user.`
-            ).sendResponse();
-        }
-
-        if (formInput['password'] !== formInput['confirm_password']) {
-            return new ApiResponse(
-                res, 406, false, 
-                `Password and confirm_password field should be same.`
-            ).sendResponse();
-        }
-
-        // Check duplicate user by username
-        try {
-            let user = null;
-            await User.findOne({username: formInput['username']}).exec()
-                .then(foundUser => {
-                    user = foundUser;
-                });
-            
-            // Jika user sudah ada
-            if (user !== null) {
-                return new ApiResponse(
-                    res, 406, false, 
-                    `Username must be unique.`
-                ).sendResponse();
-            }
-
-            userRepository.insertOne(formInput);
-            return new ApiResponse(
-                res, 201, true, 
-                'New user created.'
-            ).sendResponse();
-        } catch (err) {
-            return new ApiResponse(
-                res, 500, false, 
-                err.message
-            ).sendResponse();
-        }
+  async getUser(req, res) {
+    interface User {
+      username: string,
+      password: string,
+      img: string,
+      created_at: Date,
+      updated_at: Date,
     }
 
-    async updateUser(req, res) {
-        const userLogin = req.query.token;
-        const filter = { username: req.params.username };
-        const formInput = {
-            username: req.body.username,
-            password: req.body.password,
-            confirm_password: req.body.confirm_password,
-        }
+    const userLogin = req.session.username;
+    const username = req.params.username;
 
-        if (typeof userLogin === 'undefined') {
-            return new ApiResponse(
-                res, 406, false, 
-                'Please login first to update user.'
-            ).sendResponse();
-        }
-
-        if (formInput['password'] !== formInput['confirm_password']) {
-            return new ApiResponse(
-                res, 406, false, 
-                `Password and confirm_password field should be same.`
-            ).sendResponse();
-        }
-      
-        try {
-            // Find current user to update
-            let userToUpdate = null;
-            await User.findOne(filter).exec()
-                .then(foundUser => {
-                    userToUpdate = foundUser
-                });
-            
-            // Jika user tidak ada
-            if (userToUpdate === null) {
-                return new ApiResponse(
-                    res, 406, false, 
-                    `User not found.`
-                ).sendResponse();
-            }
-            
-            const update: any = {
-                password: req.body.password !== undefined ? req.body.password : userToUpdate.password,
-                updated_at: new Date().getTime(),
-            };
-
-            let user = await userRepository.findOneAndUpdate(filter, update);
-
-            if (user === null) {
-                return new ApiResponse(
-                    res, 404, false, 
-                    `User not found.`
-                ).sendResponse();
-            }
-
-            return new ApiResponse(
-                res, 201, true, 
-                `Success update user with username ${formInput.username}.`,
-                { user }
-            ).sendResponse();
-        } catch(err) {
-            return new ApiResponse(
-                res, 500, false, 
-                err.message
-            ).sendResponse();
-        }
+    if (typeof userLogin === 'undefined') {
+      return new ApiResponse(
+        res, 401, false,
+        `Please login first to get user.`
+      ).sendResponse();
     }
 
-    async removeUser(req, res) {
-        const _id = req.params.id;
-        const userLogin = req.query.token;
+    try {
+      let user = null;
+      await User.findOne({ username }).exec()
+        .then(foundUser => {
+          user = foundUser;
+        });
 
-        if (typeof userLogin === 'undefined') {
-            return new ApiResponse(
-                res, 406, false, 
-                'Please login first to remove user.'
-            ).sendResponse();
-        }
-        
-        try {
-            let user = await userRepository.removeUser({ _id });
-            
-            if (user === null) {
-                return new ApiResponse(
-                    res, 404, false, 
-                    'User not found.'
-                ).sendResponse();
-            }
+      if (user == null) {
+        return new ApiResponse(
+          res, 404, false,
+          `User not found.`
+        ).sendResponse();
+      }
 
-            return new ApiResponse(
-                res, 201, true, 
-                'Success deleted user.',
-                { user }
-            ).sendResponse();
-
-        } catch(err) {
-            return new ApiResponse(
-                res, 500, false, 
-                err.message
-            ).sendResponse();
-        }
+      return new ApiResponse(
+        res, 200, true,
+        `Found user.`,
+        { user }
+      ).sendResponse();
+    } catch (err) {
+      return new ApiResponse(
+        res, 500, false,
+        err.message
+      ).sendResponse();
     }
+  }
+
+  async insertUser(req, res) {
+    const userLogin: string = req.query.token;
+
+    if (typeof userLogin === 'undefined') {
+      return new ApiResponse(
+        res, 401, false,
+        `Please login first to insert new user.`
+      ).sendResponse();
+    }
+
+    const formInput: any = {
+      username: req.body.username,
+      password: req.body.password,
+      confirm_password: req.body.confirm_password
+    }
+
+    for (const input in formInput) {
+      if (formInput[input] == "") {
+        return new ApiResponse(
+          res, 406, false,
+          `Form should be filled.`
+        ).sendResponse();
+      }
+    }
+
+    if (formInput['password'] !== formInput['confirm_password']) {
+      return new ApiResponse(
+        res, 406, false,
+        `Password and confirm_password field should be same.`
+      ).sendResponse();
+    }
+
+    // Check duplicate user by username
+    try {
+      let user = null;
+      await User.findOne({ username: formInput['username'] }).exec()
+        .then(foundUser => {
+          user = foundUser;
+        });
+
+      // Jika user sudah ada
+      if (user !== null) {
+        return new ApiResponse(
+          res, 406, false,
+          `Username must be unique.`
+        ).sendResponse();
+      }
+
+      userRepository.insertOne(formInput);
+      return new ApiResponse(
+        res, 201, true,
+        'New user created.'
+      ).sendResponse();
+    } catch (err) {
+      return new ApiResponse(
+        res, 500, false,
+        err.message
+      ).sendResponse();
+    }
+  }
+
+  async updateUser(req, res) {
+    const userLogin = req.query.token;
+    const filter = { username: req.params.username };
+    const formInput = {
+      username: req.body.username,
+      password: req.body.password,
+      confirm_password: req.body.confirm_password,
+    }
+
+    if (typeof userLogin === 'undefined') {
+      return new ApiResponse(
+        res, 406, false,
+        'Please login first to update user.'
+      ).sendResponse();
+    }
+
+    if (formInput['password'] !== formInput['confirm_password']) {
+      return new ApiResponse(
+        res, 406, false,
+        `Password and confirm_password field should be same.`
+      ).sendResponse();
+    }
+
+    try {
+      // Find current user to update
+      let userToUpdate = null;
+      await User.findOne(filter).exec()
+        .then(foundUser => {
+          userToUpdate = foundUser
+        });
+
+      // Jika user tidak ada
+      if (userToUpdate === null) {
+        return new ApiResponse(
+          res, 406, false,
+          `User not found.`
+        ).sendResponse();
+      }
+
+      const update: any = {
+        password: req.body.password !== undefined ? req.body.password : userToUpdate.password,
+        updated_at: new Date().getTime(),
+      };
+
+      let user = await userRepository.findOneAndUpdate(filter, update);
+
+      if (user === null) {
+        return new ApiResponse(
+          res, 404, false,
+          `User not found.`
+        ).sendResponse();
+      }
+
+      return new ApiResponse(
+        res, 201, true,
+        `Success update user with username ${formInput.username}.`,
+        { user }
+      ).sendResponse();
+    } catch (err) {
+      return new ApiResponse(
+        res, 500, false,
+        err.message
+      ).sendResponse();
+    }
+  }
+
+  async removeUser(req, res) {
+    const _id = req.params.id;
+    const userLogin = req.query.token;
+
+    if (typeof userLogin === 'undefined') {
+      return new ApiResponse(
+        res, 406, false,
+        'Please login first to remove user.'
+      ).sendResponse();
+    }
+
+    try {
+      let user = await userRepository.removeUser({ _id });
+
+      if (user === null) {
+        return new ApiResponse(
+          res, 404, false,
+          'User not found.'
+        ).sendResponse();
+      }
+
+      return new ApiResponse(
+        res, 201, true,
+        'Success deleted user.',
+        { user }
+      ).sendResponse();
+
+    } catch (err) {
+      return new ApiResponse(
+        res, 500, false,
+        err.message
+      ).sendResponse();
+    }
+  }
 }
 
 
