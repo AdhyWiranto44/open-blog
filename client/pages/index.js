d27c597f09de22cc0ed3e206f3847e14453da8d4
Adhy Wiranto <30892092+AdhyWiranto44@users.noreply.github.com>
2022-04-23 12:50:29 +0700

    fix: search post in frontend

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,61 +1,48 @@
-import { useState, useEffect } from 'react';
-import { v4 as uuidv4 } from 'uuid';
+import { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
 
-import Tag from '../components/frontend/Tag';
-import Post from '../components/frontend/Post';
-import FrontendLayout from '../layouts/frontend';
-import { getPosts } from '../api/posts';
-import { getTags } from '../api/tags';
-
+import Tag from "../components/frontend/Tag";
+import Post from "../components/frontend/Post";
+import FrontendLayout from "../layouts/frontend";
+import { getPosts } from "../actions/posts";
+import { getTags } from "../actions/tags";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function Home() {
-    const [posts, setPosts] = useState([]);
-    const [tags, setTags] = useState([]);
+  const posts = useSelector((state) => state.posts);
+  const tags = useSelector((state) => state.tags);
+  const dispatch = useDispatch();
 
-    const handleGetPosts = () => {
-      getPosts().then(({data}) => {
-        setPosts([...data.data.posts]);
-      }).catch(err => {
-        console.log(err);
-      });
-    }
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
 
-    const handleGetTags = () => {
-      getTags().then(({data}) => {
-        setTags([...data.data.tags]);
-      }).catch(err => {
-        console.log(err);
-      });
-    }
+  useEffect(() => {
+    dispatch(getTags());
+  }, []);
 
-    useEffect(() => {
-      handleGetPosts();
-    }, []);
-
-    useEffect(() => {
-      handleGetTags();
-    }, []);
-
-    return (
-        <FrontendLayout
-            pageContent={
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-4 order-md-2 my-4 my-md-0">
-                            {tags.map(tag => {
-                                return (
-                                    <Tag key={uuidv4()} tag={tag} />
-                                );
-                            })}
-                        </div>
-                        <div className="col-md-8">
-                            {!posts.length ? <h2>Post not found</h2> : posts.map(post => {
-                                return <Post key={post._id} post={post} />
-                            })}
-                        </div>
-                    </div>
-                </div>
-            }
-        />
-    );
+  return (
+    <FrontendLayout
+      pageContent={
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4 order-md-2 my-4 my-md-0">
+              {tags.map((tag) => {
+                return <Tag key={uuidv4()} tag={tag} />;
+              })}
+            </div>
+            <div className="col-md-8">
+              {!posts.length ? (
+                <h2>Post not found</h2>
+              ) : (
+                posts.map((post) => {
+                  return <Post key={post._id} post={post} />;
+                })
+              )}
+            </div>
+          </div>
+        </div>
+      }
+    />
+  );
 }
